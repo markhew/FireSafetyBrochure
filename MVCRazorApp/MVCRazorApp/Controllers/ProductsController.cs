67216667f65d4c914cdde68aefea3cae0af1c2b7
markhew <mhj12593@gmail.com>
2017-02-03 10:31:43 +0800

    Initial Commit

---
+++ MVCRazorApp/MVCRazorApp/Controllers/ProductsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+
+using MVCRazorApp.Models;
+using System.IO;
+
+namespace MVCRazorApp.Controllers
+{
+	public class ProductsController : Controller
+	{
+
+		private ProductDBContext db = new ProductDBContext();
+
+		// GET: /Products/ 
+		public ActionResult Index()
+		{
+			return View(db.Products.ToList());
+		}
+
+		// GET: /Movies/Details/5 
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Product product = db.Products.Find(id);
+			if (product == null)
+			{
+				return HttpNotFound();
+			}
+			return View(product);
+		}
+
+		// GET: /Movies/Create 
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: /Product/Create 
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for  
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598. 
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(string Name, int Price, string Type, string Description)
+		// "ID,Name,Type,Description,Price"
+		{
+			Product product = new Product();
+			if (ModelState.IsValid)
+			{
+				byte[] imgByte = null;
+				/*
+				if (Img != null)
+				{
+					using (var binaryReader = new BinaryReader(Img.InputStream))
+					{
+						imgByte = binaryReader.ReadBytes(Img.ContentLength);
+					}
+					       
+				}*/
+
+				product.Name = Name;
+				product.Type = Type;
+				product.Description = Description;
+				product.Price = Price;
+				product.ProductImg = imgByte;
+
+				db.Products.Add(product);
+
+				// Test debug on Mac
+
+
+
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(product);
+		}
+
+		// GET: /Product/Edit/5 
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Product product = db.Products.Find(id);
+			if (product == null)
+			{
+				return HttpNotFound();
+			}
+			return View(product);
+		}
+
+		// POST: /Product/Edit/5 
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for  
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598. 
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "Name,Type,Description,Price")] Product product)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(product).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(product);
+		}
+
+		// GET: /Movies/Delete/5 
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Product product = db.Products.Find(id);
+			if (product == null)
+			{
+				return HttpNotFound();
+			}
+			return View(product);
+		}
+
+		// POST: /Product/Delete/5 
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Product product = db.Products.Find(id);
+			db.Products.Remove(product);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+	}
+}
---
+++ MVCRazorApp/MVCRazorApp/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace MVCRazorApp.Models
+{
+	public class Product
+	{
+
+
+
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+		public string Type { get; set; }
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		[DisplayName("Image")]
+		public byte[] ProductImg { get; set; }
+
+
+
+
+	}
+
+	public partial class ProductDBContext : DbContext
+	{
+		public ProductDBContext() : base(nameOrConnectionString: "MyDatabaseContext") { }
+
+		public DbSet<Product> Products { get; set; }
+
+	}
+}
